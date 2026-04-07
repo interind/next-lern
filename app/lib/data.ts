9bb5f9411755ffee5fbe8360fae7a1c2f5509f2a
Alexandr Bannykh <aba@lemnos.ru>
2026-04-07 13:00:17 +0500

    chapter#9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,15 +10,7 @@ import { formatCurrency, sql } from "./utils";
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    console.log("Data fetch completed after 3 seconds.");
 
     return data;
   } catch (error) {
@@ -36,7 +28,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.map((invoice) => ({
+    const latestInvoices = data?.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
